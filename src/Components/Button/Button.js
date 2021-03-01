7bd306797948c86383e388287bbf52cef054e084
Liliia Pedorych <liliapedorych@ukr.net>
2021-03-01 22:27:38 +0200

    Додає початкові файли

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// const Button = () => {
+//   return <button type="button"> Load more</button>;
+// };
+
+// export default Button;
+
+// window.scrollTo({
+//   top: document.documentElement.scrollHeight,
+//   behavior: 'smooth',
+// });
---
+++ src/Components/ImageGallery/ImageGallery.js
@@ -0,0 +1,13 @@
+//import React from 'react';
+// import PropTypes from 'prop-types';
+// import ImageGalleryItem from "./ImageGalleryItem";
+
+// const ImageGallery = () => {
+//   return (
+//     <ul className="ImageGallery">
+//       <ImageGalleryItem />
+//     </ul>
+//   );
+// };
+
+// export default ImageGallery;
